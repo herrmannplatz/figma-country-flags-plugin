cbe03fe9c119d1d7f0c2a91ae400ac8d90f7839f
herrmannplatz <rene@herrmannplatz.de>
2019-09-08 22:20:32 +0200

    Clean up utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
-export const includes = (a: String, b: String) =>
+export const includes = (a: string, b: string) =>
   a.toLowerCase().includes(b.toLowerCase());
 
-export const decodeDataURI = dataURI => {
+export const decodeDataURI = (dataURI: string) => {
   const [, base64] = dataURI.split("data:image/svg+xml;base64,");
   return window.atob(base64);
 };
