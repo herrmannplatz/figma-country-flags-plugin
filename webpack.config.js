21c5dd335ade55da5dc65a467fe7c77e1d9cd277
Panayiotis Lipiridis <lipiridis@gmail.com>
2019-09-20 20:05:37 +0200

    npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = (env, argv) => ({
   mode: argv.mode === 'production' ? 'production' : 'development',
@@ -42,4 +42,4 @@ module.exports = (env, argv) => ({
     }),
     new HtmlWebpackInlineSourcePlugin(),
   ],
-})
+});
