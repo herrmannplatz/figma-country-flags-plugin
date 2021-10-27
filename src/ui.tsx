a5d82f839811ea97ccb8cccf6f59b79e13d695c8
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-10-27 19:58:20 +0300

    update

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,22 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
 // figma-plugin-cs
-import './lib/figma-plugin-ds/icon-input';
-import './lib/figma-plugin-ds/select-menu';
-import './lib/figma-plugin-ds/index.css';
+import "./lib/figma-plugin-ds/icon-input";
+import "./lib/figma-plugin-ds/select-menu";
+import "./lib/figma-plugin-ds/index.css";
 
-import SearchInput from './components/SearchInput';
-import Settings from './components/Settings';
+import SearchInput from "./components/SearchInput";
+import Settings from "./components/Settings";
 
-import {countries} from './lib/flag-icons';
-import {includes, decodeDataURI} from './lib/utils';
+import { countries } from "./lib/flag-icons";
+import { includes, decodeDataURI } from "./lib/utils";
 
-import './ui.css';
+import "./ui.css";
 
 const App = () => {
-  const [query, setQuery] = useState('');
-  const [ratio, setRatio] = useState('4x3');
+  const [query, setQuery] = useState("");
+  const [ratio, setRatio] = useState("4x3");
 
   useEffect(() => {
     // need to be called when content is rendered
@@ -24,31 +24,35 @@ const App = () => {
     (window as any).iconInput.init();
   }, []);
 
-  const flags = countries.filter(({name, code}) => {
-    const matchesQuery = includes(name, query) || query === code;
+  const flags = countries.filter(({ name, code }) => {
+    const matchesQuery =
+      includes(name, query) || query === code;
     return matchesQuery;
   });
 
-  const onCreate = (flag) => {
-    const svg = decodeDataURI(flag['flag_' + ratio]);
-    parent.postMessage({pluginMessage: {type: 'create-flag', svg}}, '*');
+  const onCreate = flag => {
+    const svg = decodeDataURI(flag["flag_" + ratio]);
+    parent.postMessage({ pluginMessage: { type: "create-flag", svg } }, "*");
   };
 
   return (
     <div>
       <div className="header">
         <SearchInput onChange={setQuery} />
-        <Settings ratio={ratio} onChangeRatio={setRatio} />
+        <Settings
+          ratio={ratio}
+          onChangeRatio={setRatio}
+        />
       </div>
       <div className="content">
         <div className="flags">
-          {flags.map((flag) => (
+          {flags.map(flag => (
             <div
               onClick={() => onCreate(flag)}
               className="flag"
               key={flag.code}
             >
-              <img src={flag['flag_' + ratio]} />
+              <img src={flag["flag_" + ratio]} />
               <div className="flag-name">{flag.name}</div>
             </div>
           ))}
@@ -58,4 +62,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('react-page'));
+ReactDOM.render(<App />, document.getElementById("react-page"));
