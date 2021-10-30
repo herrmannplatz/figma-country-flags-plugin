4b6491764eaf4175a698b1963df053827ee2b3ed
Rene Herrmann <reneherrmann@MBP-von-Rene.fritz.box>
2021-10-30 23:21:22 +0200

    Use country.json from package

--- src/lib/flag-icons/index.ts
+++ src/lib/flag-icons/index.ts
@@ -1,14 +1,14 @@
-const countryJSON = require("./country");
+const countryJSON = require("flag-icon-css/country.json");
 const cache = {};
 
 function importAll(r) {
   r.keys().forEach(key => (cache[key] = r(key)));
 }
 
-importAll(require.context("flag-icon-css/flags", true, /\.svg$/));
+importAll(require.context("flag-icon-css", true, /flags\/.*\.svg$/));
 
 export const countries = countryJSON.map(country => ({
   ...country,
-  flag_1x1: cache[country.flag_1x1].default,
-  flag_4x3: cache[country.flag_4x3].default
+  flag_1x1: cache[`./${country.flag_1x1}`].default,
+  flag_4x3: cache[`./${country.flag_4x3}`].default
 }));
