d8624a8d3d8695f1417f2fd5f3b310e963631b49
herrmannplatz <rene@herrmannplatz.de>
2025-02-16 21:42:51 +0100

    Fix us flag

---
+++ scripts/fix-flags.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+inkscape --actions="select-all:all;object-stroke-to-path;export-plain-svg" --export-filename=node_modules/flag-icons/flags/4x3/us.svg node_modules/flag-icons/flags/4x3/us.svg
